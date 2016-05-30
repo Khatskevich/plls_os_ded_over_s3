8e015dcd2cf13da72fa2144a3d38b418fb1c5dad
AlexeyK <avkhatskevich@gmail.com>
2016-05-30 07:42:25 +0300

    restoring data from disc

---
+++ src/main/java/com/plls/os/dedublicated/server/storage/StorageOverLocalDiskSimplest.java
@@ -0,0 +1,34 @@
+package com.plls.os.dedublicated.server.storage;
+
+import com.plls.os.dedublicated.server.data.Chunk;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.TreeMap;
+
+public class StorageOverLocalDiskSimplest implements StorageServer {
+    @Override
+    public void fillChunksData(TreeMap<Long,Chunk> chunks, Chunk representative) throws Exception {
+        for(Long start : chunks.keySet() ){
+            Chunk c = chunks.get(start);
+            FileInputStream fis = new FileInputStream("/tmp/"+c.id);
+            byte[] b = new byte[(int) c.size];
+            fis.read(b,0, (int) c.size);
+            c.data = b;
+            fis.close();
+        }
+    }
+
+    @Override
+    public void saveChunks(TreeMap<Long,Chunk> chunks, Chunk representative) throws Exception {
+        for( Chunk c : chunks.values() ){
+            FileOutputStream fos = new FileOutputStream("/tmp/"+c.id);
+            try {
+                fos.write(c.data);
+            }
+            finally {
+                fos.close();
+            }
+        }
+    }
+}
