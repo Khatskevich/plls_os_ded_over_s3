0e8224e7e20d66431bdac0295ac8c5e3cb0d8280
AlexeyK <avkhatskevich@gmail.com>
2016-05-30 07:54:09 +0300

    server basics

--- src/main/java/com/plls/os/dedublicated/server/Server.java
+++ src/main/java/com/plls/os/dedublicated/server/Server.java
@@ -1,7 +1,15 @@
 package com.plls.os.dedublicated.server;
 
-/**
- * Created by alexeykhatskevich on 29.05.16.
- */
+import com.plls.os.dedublicated.server.data.OSDChunkedObject;
+import com.plls.os.dedublicated.server.meta.MetaServer;
+import com.plls.os.dedublicated.server.storage.StorageServer;
+
 public class Server {
+    MetaServer ms;
+    StorageServer ss;
+    Server(MetaServer ms, StorageServer ss){
+    }
+    OSDChunkedObject getFileByName(String name){
+      return null;
+    }
 }
